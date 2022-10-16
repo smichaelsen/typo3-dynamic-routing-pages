311a98d0201f4e871dd12f6c42f766ae7625b09d
Sebastian Michaelsen <sebastian@michaelsen.io>
2022-10-16 10:54:39 +0200

    chore: mark as compatible with TYPO3 v12

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || defined('TYPO3_MODE') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Configuration\SiteConfiguration::class] = [
     'className' => \Smic\DynamicRoutingPages\Xclass\SiteConfiguration::class,
