f92dc9b3aebadd5f7ba0ec1b95d7cd73a04a96ee
Timo Webler <timo.webler@dkd.de>
2023-06-27 12:18:29 +0200

    [Task] Add support for TYPO3 12 (#17)
    Remove support for TYPO3 10

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,13 @@
 <?php
 
-defined('TYPO3_MODE') || die();
+use TYPO3\CMS\Core\Configuration\SiteConfiguration;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Configuration\SiteConfiguration::class] = [
-    'className' => \Smic\DynamicRoutingPages\Xclass\SiteConfiguration::class,
-];
+defined('TYPO3') || die();
+
+if (GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() < 12) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][SiteConfiguration::class] = [
+        'className' => \Smic\DynamicRoutingPages\Xclass\SiteConfiguration::class,
+    ];
+}
