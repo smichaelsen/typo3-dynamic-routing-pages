04aff1fadef689f120cd3f5a6a337935b63e7d11
Sebastian Michaelsen <sebastian@michaelsen.io>
2024-04-30 09:58:24 +0200

    chore: use events in TYPO3 v12

---
+++ Classes/EventListener/ModifySiteConfigurationOnLoading.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smic\DynamicRoutingPages\EventListener;
+
+use Smic\DynamicRoutingPages\ConfigurationModifier;
+use TYPO3\CMS\Core\Configuration\Event\SiteConfigurationLoadedEvent;
+
+class ModifySiteConfigurationOnLoading
+{
+    public function __invoke(SiteConfigurationLoadedEvent $event): SiteConfigurationLoadedEvent
+    {
+        $siteConfiguration = $event->getConfiguration();
+        $siteConfiguration = ConfigurationModifier::modifySiteConfiguration($siteConfiguration);
+        $event->setConfiguration($siteConfiguration);
+
+        return $event;
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,8 @@
 
 defined('TYPO3') || defined('TYPO3_MODE') || die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Configuration\SiteConfiguration::class] = [
-    'className' => \Smic\DynamicRoutingPages\Xclass\SiteConfiguration::class,
-];
+if (version_compare(\TYPO3\CMS\Core\Utility\VersionNumberUtility::getCurrentTypo3Version(), '12.0', '<=')) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Configuration\SiteConfiguration::class] = [
+        'className' => \Smic\DynamicRoutingPages\Xclass\SiteConfiguration::class,
+    ];
+}
